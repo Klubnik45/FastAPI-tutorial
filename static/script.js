adf13d615226154d5b7bbc88da76dba600355d55
AlexTab45 <tishkina4554@gmail.com>
2025-03-17 21:44:51 +0300

    17.03.2025

--- static/script.js
+++ static/script.js
@@ -1,11 +1,14 @@
+let closeBtn = document.querySelector("#closeBtn");
+let saveBtn = document.querySelector("#saveBtn");
 let addPage = document.getElementById("add-book");
 let addBtn = document.getElementById("addBtn");
+let updatePage = document.querySelectorAll("#book-list>li>form");
+
+
 addPage.classList.add("hide-page");
 
 addBtn.addEventListener("click", function(){
     addPage.classList.remove("hide-page");
-    let closeBtn = document.getElementById("closeBtn");
-    closeBtn.addEventListener("click", function(){
-        addPage.classList.add("hide-page");
-    });
 });
+
+console.log(updatePage);
